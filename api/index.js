b1252bb8f14f83171e7ddf12799427b63e5fb0d1
George Cheng <Gerhut@GMail.com>
2020-07-20 17:41:19 +0800

    Try creating the collection and the index on error
    Fixes #1

--- api/index.js
+++ api/index.js
@@ -1,37 +1,83 @@
+import assert from 'assert'
 import { Client, query, errors } from 'faunadb'
 import { makeBadge } from 'badge-maker'
+
+const { FAUNADB_SECRET } = process.env
+const COLLECTION_NAME = 'visitors'
+const INDEX_NAME = 'visitors_by_url'
+
+assert(FAUNADB_SECRET != null, 'No FAUNADB_SECRET environment variable provided.')
 
 /**
  * @param {import('http').IncomingMessage} request
  * @param {import('http').ServerResponse} response
  */
-export default async function (request, response) {
+export default async function api (request, response) {
   const { url } = request
   try {
     const client = new Client({ secret: process.env.FAUNADB_SECRET })
-    const visitors = await client.query(
-      query.Select(
-        ['data', 'visitors'],
-        query.Let(
-          { match: query.Match(query.Index('visitors_by_url'), url) },
-          query.If(
-            query.Exists(query.Var('match')),
-            query.Let(
-              { get: query.Get(query.Var('match')) },
-              query.Update(query.Select('ref', query.Var('get')), {
-                data: {
-                  visitors: query.Add(
-                    query.Select(['data', 'visitors'], query.Var('get')),
-                    1
-                  )
-                }
-              })
-            ),
-            query.Create(query.Collection('visitors'), { data: { url, visitors: 1 } })
+    let visitors
+    try {
+      visitors = await client.query(
+        query.Select(
+          ['data', 'visitors'],
+          query.Let(
+            { match: query.Match(query.Index(INDEX_NAME), url) },
+            query.If(
+              query.Exists(query.Var('match')),
+              query.Let(
+                { get: query.Get(query.Var('match')) },
+                query.Update(query.Select('ref', query.Var('get')), {
+                  data: {
+                    visitors: query.Add(
+                      query.Select(['data', 'visitors'], query.Var('get')),
+                      1
+                    )
+                  }
+                })
+              ),
+              query.Create(query.Collection(COLLECTION_NAME), { data: { url, visitors: 1 } })
+            )
           )
         )
       )
-    )
+    } catch (error) {
+      if (
+        error instanceof errors.FaunaHTTPError && (
+          error.message === 'invalid ref' ||
+          error.message === 'validation failed'
+        )
+      ) {
+        const createdCollection = await client.query(
+          query.If(
+            query.Not(query.Exists(query.Collection(COLLECTION_NAME))),
+            query.CreateCollection({
+              name: COLLECTION_NAME
+            }),
+            null
+          )
+        )
+        const createdIndex = await client.query(
+          query.If(
+            query.Not(query.Exists(query.Index(INDEX_NAME))),
+            query.CreateIndex({
+              name: INDEX_NAME,
+              source: query.Collection(COLLECTION_NAME),
+              terms: [
+                { field: ['data', 'url'] }
+              ],
+              unique: true
+            }),
+            null
+          )
+        )
+        if (createdCollection != null || createdIndex != null) {
+          return api(request, response)
+        }
+      } else {
+        throw error
+      }
+    }
     const body = makeBadge({
       label: 'visitors',
       message: String(visitors),
